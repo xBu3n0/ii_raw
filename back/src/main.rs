9d26f5c984ee75a2209be038e040481f62f5655c
Matheus Bueno <matheusbartkev@gmail.com>
2024-05-25 00:28:45 -0300

    melhorando o padrão de retorno das funções

--- back/src/main.rs
+++ back/src/main.rs
@@ -10,17 +10,28 @@ use rocket::{http::Status, serde::json::Json};
 #[macro_use]
 extern crate rocket;
 
+#[catch(404)]
+fn not_found() -> String {
+    format!("404 - Not found")
+}
+
+#[catch(409)]
+fn conflict() -> String {
+    format!("409 - The user with this `id` already exists")
+}
+
 #[get("/users")]
-fn get_users() -> Json<Vec<User>> {
+// https://api.rocket.rs/master/rocket/response/status/
+async fn get_users() -> Result<(Status, Json<Vec<User>>), Status> {
     let conn = &mut establish_connection();
 
     let qtd = users.load(conn).unwrap() as Vec<User>;
 
-    Json(qtd)
+    Ok((Status::Ok, Json(qtd)))
 }
 
 #[post("/users", data = "<data>")]
-fn create_user(data: Json<User>) -> (Status, Result<Json<User>, String>) {
+async fn create_user(data: Json<User>) ->  Result<(Status, Json<User>), Status> {
     let conn = &mut establish_connection();
 
     let user: User = data.into();
@@ -29,22 +40,24 @@ fn create_user(data: Json<User>) -> (Status, Result<Json<User>, String>) {
         .values(user)
         .get_result::<User>(conn)
     {
-        Ok(user) => (Status::Created, Ok(Json::from(user))),
-        Err(e) => (Status::Conflict, Err(e.to_string()))
+        Ok(user) => Ok((Status::Created, Json::from(user))),
+        Err(_) => Err(Status::Conflict)
     }
 }
 
 #[get("/users/<index>")]
-fn get_user(index: i32) -> (Status, Json<Result<User, String>>) {
+async fn get_user(index: i32) -> Result<(Status, Json<User>), Status> {
     let conn = &mut establish_connection();
 
     match users::table.filter(id.eq(index)).select(User::as_select()).first(conn) {
-        Ok(user) => (Status::Ok, Json(Ok(user))),
-        Err(err) => (Status::NotFound, Json(Err(err.to_string())))
+        Ok(user) => Ok((Status::Ok, Json(user))),
+        Err(_) => Err(Status::NotFound)
     }
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/api/", routes![get_users, get_user, create_user])
+    rocket::build()
+    .mount("/api/", routes![get_users, get_user, create_user])
+    .register("/", catchers![not_found, conflict])
 }
